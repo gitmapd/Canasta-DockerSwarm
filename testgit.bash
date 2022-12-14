da260a4d322533b024a0df36d2e807736bd4a7a9
gitmapd <50059062+gitmapd@users.noreply.github.com>
2022-12-14 00:22:01 +0000

    Update testgit.bash

--- testgit.bash
+++ testgit.bash
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
 # check if git is installed
 git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 if [ $GIT_IS_AVAILABLE -ne 0 ]; 
-then echo "${RED}Git was not found, please install before continuing.${NC}";
+then echo "Git was not found, please install before continuing.";
      exit; 
 else
-     echo "${GREEN}Git was found on the system${NC}"
+     echo "Git was found on the system"
 fi
